840898721632a278295d0f0dc7cc2d09642650de
Kratul Singh <149990041+kratul9563@users.noreply.github.com>
2024-01-30 06:51:18 +0530

    Final Commit

--- frontend/src/component/layout/Footer/Footer.js
+++ frontend/src/component/layout/Footer/Footer.js
@@ -17,7 +17,7 @@ const Footer = () => {
         <h1>ECOMMERCE.</h1>
         <p>High Quality is our first priority</p>
 
-        <p>Copyrights 2021 &copy; Kratul Singh</p>
+        <p>Copyrights 2024 &copy; Kratul Singh</p>
       </div>
 
       <div className="rightFooter">
